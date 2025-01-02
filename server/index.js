2cd402b6bfe697cd2b3a87b4e0e5a167749fd51d
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-03 00:23:45 +0100

    add routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const blogRouter = require("./routes/blog.route");
 
 require("./db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/blogs", blogRouter);
 
 app.use("/api", (req, res) => {
   res.send("Hello Express");
