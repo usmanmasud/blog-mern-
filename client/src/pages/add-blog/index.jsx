cc383a8eb0f0fae11549900de227e27d85f8943c
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-03 01:00:40 +0100

    add blog form

--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import classes from "./styles.module.css";
 
 export default function AddNewBlog() {
     return (
-        <div>
+        <div className={classes.wrapper}>
             <h1>Add a blog</h1>
+            <div className={classes.formWrapper}>
+                <input
+                    name="title"
+                    placeholder="Enter Blog Title"
+                    id="title"
+                    type="text"
+                />
+                <textarea name="description" placeholder="Enter Blog Description" id="description" />
+                <button>Add New Blog</button>
+            </div>
         </div>
-    )
+    );
 }
