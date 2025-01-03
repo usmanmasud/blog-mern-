fc27d7aa0612d60e14ad340c0543ef4d47238074
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-04 00:24:44 +0100

    displaying blogs

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,41 +1,40 @@
-
-import React, { useContext, useEffect } from 'react'
-import { GlobalContex } from '../../contex/index'
-import axios from 'axios'
+import React, { useContext, useEffect } from "react";
+import { GlobalContex } from "../../contex/index";
+import axios from "axios";
 
 export default function Home() {
-    const { blogList, setBlogList, pendeng, setPending } = useContext(GlobalContex);
-
-
+    const { blogList, setBlogList, pendeng, setPending } =
+        useContext(GlobalContex);
     const fetchListOfBlogs = async () => {
-        setPending(true)
-        const response = await axios.get('http://localhost:5000/api/blogs');
+        setPending(true);
+        const response = await axios.get("http://localhost:5000/api/blogs");
         const result = await response.data;
 
-
         if (result && result.blogList && result.blogList.length) {
-            setBlogList(result.blogList)
-            setPending(false)
+            setBlogList(result.blogList);
+            setPending(false);
         }
-    }
-
+    };
 
     useEffect(() => {
-        fetchListOfBlogs()
-    }, [])
+        fetchListOfBlogs();
+    }, []);
 
-    return
-    <h1>Blog Lists</h1>
-    {
-        pendeng ? <h1>Loading..., please wait!!</h1> :
-            <div>
-                {
-                    blogList.map(item => <div key={item.id}>
-                        <p>{item.title}</p>
-                        <p>{item.description}</p>
-                    </div>)
-                }
-            </div>
-    }
-
+    return (
+        <div>
+            <h1>Blog Lists</h1>
+            {pendeng ? (
+                <h1>Loading..., please wait!!</h1>
+            ) : (
+                <div>
+                    {blogList.map((item) => (
+                        <div key={item.id}>
+                            <p>{item.title}</p>
+                            <p>{item.description}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
