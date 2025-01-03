fe66df92aa8b9546841d053299672af2419103bd
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-03 01:02:08 +0100

    adding context

--- client/src/contex/index.jsx
+++ client/src/contex/index.jsx
@@ -1,9 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContex = createContext(null);
 
 export default function GlobalState({ children }) {
+    const [formData, setFormData] = useState({
+        title: '',
+        description: ''
+    })
 
 
-    return <GlobalContex.Provider>{children}</GlobalContex.Provider>
+    return <GlobalContex.Provider value={{ formData, setFormData }}>{children}</GlobalContex.Provider>
 }
