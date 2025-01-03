3465d39c0994c5a4de385a5f9f1e07d610b8c6c4
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-03 01:07:35 +0100

    ..

--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./styles.module.css";
+import { GlobalContex } from "../../contex";
 
 export default function AddNewBlog() {
+    const { formData, setFormData } = useContext(GlobalContex);
+
+    console.log(formData)
+
+
+    const handleSaveBlog = async () => {
+
+    }
+
     return (
         <div className={classes.wrapper}>
             <h1>Add a blog</h1>
@@ -11,9 +21,19 @@ export default function AddNewBlog() {
                     placeholder="Enter Blog Title"
                     id="title"
                     type="text"
+                    value={formData.title}
+                    onChange={(e) => setFormData({ ...formData, title: e.target.value })}
                 />
-                <textarea name="description" placeholder="Enter Blog Description" id="description" />
-                <button>Add New Blog</button>
+                <textarea
+                    value={formData.description}
+                    name="description"
+                    placeholder="Enter Blog Description"
+                    id="description"
+                    onChange={(e) =>
+                        setFormData({ ...formData, description: e.target.value })
+                    }
+                />
+                <button onClick={handleSaveBlog}>Add New Blog</button>
             </div>
         </div>
     );
